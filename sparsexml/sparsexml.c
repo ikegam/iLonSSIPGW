5d26826066f043b58b2966b394107f904016f417
Hiroyuki Ikegami <ikegam@mixallow.net>
2013-09-04 09:51:21 +0900

    add new xml parser library (sparsexml)

---
+++ sparsexml/sparsexml.c
@@ -0,0 +1,158 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "sparsexml.h"
+
+unsigned char priv_sxml_change_parser_state(SXMLParser* parser, enum SXMLParserState state) {
+  static unsigned int index = 0;
+  unsigned char ret;
+  int i;
+
+  if (strlen(parser->buffer) > 0) {
+    if (parser->state == IN_TAG && state == IN_CONTENT) {
+      ret = parser->tag_func(parser->buffer);
+    } else if (parser->state == IN_TAG && state == IN_TAG ) {
+      ret = parser->tag_func(parser->buffer);
+    } else if (parser->state == IN_TAG && state == IN_ATTRIBUTE_KEY) {
+      ret = parser->tag_func(parser->buffer);
+    } else if (parser->state == IN_CONTENT && state == IN_TAG) {
+      ret = parser->content_func(parser->buffer);
+    } else if (parser->state == IN_ATTRIBUTE_KEY && state == IN_ATTRIBUTE_VALUE) {
+      ret = parser->attribute_key_func(parser->buffer);
+    } else if (parser->state == IN_ATTRIBUTE_VALUE && state == IN_TAG) {
+      ret = parser->attribute_value_func(parser->buffer);
+    }
+  }
+
+  parser->bp = 0;
+  parser->buffer[0] = '\0';
+
+  parser->state = state;
+
+  return ret;
+
+}
+
+void sxml_init_parser(SXMLParser* parser) {
+  int i;
+
+  parser->state = INITIAL;
+  parser->bp = 0;
+  parser->buffer[0] = '\0';
+  for (i=0; i<10; i++) {
+    parser->elements[i][0] = '\0';
+  }
+
+}
+
+void sxml_register_func(SXMLParser* parser, void* open, void* content, void* attribute_key, void* attribute_value) {
+  parser->tag_func = open;
+  parser->content_func = content;
+  parser->attribute_value_func = attribute_value;
+  parser->attribute_key_func = attribute_key;
+}
+
+unsigned char sxml_run_parser(SXMLParser* parser, char * xml) {
+
+  unsigned char ret = SXMLParserContinue;
+
+  do {
+    switch (parser->state) {
+      case INITIAL:
+        switch (*xml) {
+          case '<':
+            ret = priv_sxml_change_parser_state(parser, IN_TAG);
+            continue;
+            break;
+        }
+        break;
+      case IN_TAG:
+        switch (*xml) {
+          case '>':
+            ret =  priv_sxml_change_parser_state(parser, IN_CONTENT);
+            continue;
+            break;
+          case ' ':
+            ret = priv_sxml_change_parser_state(parser, IN_ATTRIBUTE_KEY);
+            continue;
+            break;
+        }
+        break;
+      case IN_ATTRIBUTE_KEY:
+        switch (*xml) {
+          case '>':
+            ret = priv_sxml_change_parser_state(parser, IN_CONTENT);
+            continue;
+            break;
+          case '=':
+            xml++;
+            ret = priv_sxml_change_parser_state(parser, IN_ATTRIBUTE_VALUE);
+            continue;
+            break;
+        }
+      case IN_ATTRIBUTE_VALUE:
+        switch (*xml) {
+          case '"':
+            ret = priv_sxml_change_parser_state(parser, IN_TAG);
+            continue;
+            break;
+        }
+        break;
+      case IN_CONTENT:
+        switch (*xml) {
+          case '<':
+            ret = priv_sxml_change_parser_state(parser, IN_TAG);
+            continue;
+            break;
+        }
+        break;
+    }
+    parser->buffer[parser->bp++] = *xml;
+    parser->buffer[parser->bp] = '\0';
+
+#ifdef __DEBUG1
+    printf("State:%d Buffer:%s\r\n", parser->state, parser->buffer);
+#endif
+
+  } while (*(xml++) != '\0' && ret == SXMLParserContinue);
+
+  if (ret == SXMLParserStop) {
+    return SXMLParserInterrupted;
+  }
+
+  return SXMLParserComplete;
+
+}
+
+// <?xml version="1.0" encoding="UTF-8"?><env:Envelope xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:ilon="http://wsdl.echelon.com/web_services_ns/ilon100/v4.0/message/" xmlns:env="http://schemas.xmlsoap.org/soap/envelope/"><env:Body><ilon:Read><iLonItem><Item><UCPTname>Net/LON/iLON App/Digital Output 1/nviClaValue_1</UCPTname></Item></iLonItem></ilon:Read></env:Body></env:Envelope>
+
+/*
+unsigned char storage[10][1024];
+unsigned char onName=0x00;
+
+void tag_open(char *tagname) {
+  printf("tag: %s\r\n", tagname);
+}
+
+void tag_content(char *content) {
+  printf("content: %s\r\n", content);
+}
+
+void key(char *name) {
+  printf("attribute key: %s\n", name);
+}
+
+void value(char *name) {
+  printf("attribute value: %s\n", name);
+}
+
+void main (void) {
+  unsigned char xml[] = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><env:Envelope xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:ilon=\"http://wsdl.echelon.com/web_services_ns/ilon100/v4.0/message/\" xmlns:env=\"http://schemas.xmlsoap.org/soap/envelope/\"><env:Body><ilon:Read><iLonItem><Item><UCPTname>Net/LON/iLON App/Digital Output 1/nviClaValue_1</UCPTname></Item></iLonItem></ilon:Read></env:Body></env:Envelope>";
+
+  SXMLParser parser;
+  sxml_init_parser(&parser);
+  sxml_register_func(&parser, &tag_open, &tag_content, &key, &value, (unsigned char*)storage);
+  sxml_run_parser(&parser, xml);
+}
+*/
---
+++ sparsexml/sparsexml.h
@@ -0,0 +1,37 @@
+#ifndef __SXMLParser__
+#define __SXMLParser__
+
+enum SXMLParserState {
+  INITIAL,
+  IN_TAG,
+  IN_ATTRIBUTE_KEY,
+  IN_ATTRIBUTE_VALUE,
+  IN_CONTENT
+};
+
+#define SXMLParserContinue 0x00
+#define SXMLParserStop 0x01
+
+#define SXMLParserComplete 0x02
+#define SXMLParserInterrupted 0x03
+
+
+typedef struct __SXMLParser {
+  enum SXMLParserState state;
+
+  unsigned char buffer[1024];
+  unsigned char elements[10][1024];
+  unsigned int bp;
+
+  unsigned char (*tag_func)(char *);
+  unsigned char (*content_func)(char *);
+  unsigned char (*attribute_value_func)(char *);
+  unsigned char (*attribute_key_func)(char *);
+} SXMLParser;
+
+unsigned char priv_sxml_change_parser_state(SXMLParser* parser, enum SXMLParserState state);
+void sxml_init_parser(SXMLParser* parser);
+void sxml_register_func(SXMLParser* parser, void* tag, void* content, void* attribute_key, void* attribute_value);
+unsigned char sxml_run_parser(SXMLParser* parser, char * xml);
+
+#endif
